0d36b769a7cbf623bae3ad67918edb6baedb9a20
danieldmonteiro <danieldmonteiro@gmail.com>
2022-01-20 17:33:14 -0300

    ÜC8er1

---
+++ resolucao-problemas/uc8er1.js
@@ -0,0 +1,23 @@
+var DataEvento = [21/01/2022, 22/01/2022, 23/01/2022, 24/01/2022, 25/01/2022]
+
+    if(DataEvento<21/01/2022){
+        console.log("Data inválida")
+    }else {
+        console.log("Evento permitido")
+    }
+        
+var IdadeParticipante = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+    if(IdadeParticipante<18){
+        console.log("Cadastro não permitido, pela idade")
+    }else {
+        console.log("Cadastro permitido")
+    }
+
+var Participantes = ["Daniel", "Pedro", "Raoni", "Jessica", "Sandra"]
+
+    if(Participantes.lenght <= 100){
+        console.log("É possível cadastrar")
+    }else {
+        console.log("Cadastro não permitido, limite de participantes atingido")
+    }
